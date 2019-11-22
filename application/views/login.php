db19b02b2e626be01eb729c4ff9e805a67e03198
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2019-11-22 13:01:31 -0600

    css

---
+++ application/views/login.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Inicio / Registro</title>
+	<link rel="stylesheet" href="<?= base_url(); ?>css/main.css">
+</head>
+<body>
+
+<div class="container login">
+	
+</div>
+
+</body>
+</html>
---
+++ application/views/sidemenu.php
@@ -0,0 +1,7 @@
+<div class="menu-container">
+	<div class="menu-option">Opcion 1</div>
+	<div class="menu-option">Opcion 2</div>
+	<div class="menu-option">Opcion 3</div>
+	<div class="menu-option">Opcion 4</div>
+	<div class="menu-option">Opcion 5</div>
+</div>
